a49dea53130e4691b5e65d8acc424b398c731383
DesarrolloPruebas <132235054+DesarrolloPruebas@users.noreply.github.com>
2023-05-14 13:38:40 -0600

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1684092708,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/fondo-sheet0.png",
+		"images/piso.png",
+		"images/personaje-sheet0.png",
+		"images/personaje-sheet1.png",
+		"images/personaje-sheet2.png",
+		"images/bloquesuperior-sheet0.png",
+		"images/bloquemedio-sheet0.png",
+		"images/bloqueinferior-sheet0.png",
+		"images/fondoinicio-sheet0.png",
+		"media/audio.m4a",
+		"media/audio.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
